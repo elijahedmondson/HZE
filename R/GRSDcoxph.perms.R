53f9e7423f28a4a5b0241dda827a21f3cc3d0750
elijahedmondson <elijah.edmondson@gmail.com>
2016-04-06 10:56:36 -0600

    CoxPH Permutation functions Fixed

---
+++ R/GRSDcoxph.perms.R
@@ -0,0 +1,106 @@
+#' @title Permutation analysis for CoxPH model in HZE.
+#' @author Elijah F Edmondson, \email{elijah.edmondson@@gmail.com}
+#' @export
+
+GRSDcoxph.perms = function(perms, chr = 1:19, Xchr = FALSE,
+                           pheno, pheno.col, days.col, probs, K, addcovar,
+                           markers, snp.file, outdir = "/home/ubuntu",
+                           tx = "", sanger.dir = "home/ubuntu/HS.sanger.files/") {
+        begin <- Sys.time()
+        begin
+
+        samples = intersect(rownames(pheno), rownames(probs))
+        samples = intersect(samples, rownames(addcovar))
+        samples = intersect(samples, rownames(K[[1]]))
+        stopifnot(length(samples) > 0)
+
+        pheno = pheno[samples,,drop = FALSE]
+        addcovar = addcovar[samples,,drop = FALSE]
+        probs = probs[samples,,,drop = FALSE]
+
+        # DEFINE surv #
+
+        file.prefix = paste(tx, pheno.col, sep = "_")
+
+        plot.title = paste(tx, pheno.col, sep = " ")
+        print(paste(plot.title, "Permutation Analysis:", Sys.time()))
+
+        # COX PH MODEL #
+        surv = Surv(pheno[,days.col], pheno[,pheno.col])
+        fit = survfit(surv ~ addcovar)
+        plot(fit, col = 1:2, las = 1, main = plot.title)
+        legend("bottomleft", col = 1:2, lty = 1, legend = c("female", "male"))
+        mod = coxph(surv ~ addcovar)
+        text(x = 25, y = 0.15, labels = paste("p =", format(anova(mod)[2,4],
+                                                            digits = 2)), adj = 0)
+
+        for(i in 1:length(K)) {
+                K[[i]] = K[[i]][samples, samples]
+        } # for(i)
+
+        chrs = c(1:19, "X")
+        data = vector("list", length(chrs))
+        names(data) = chrs
+        for(i in 1:length(chrs)) {
+
+                rng = which(markers[,2] == chrs[i])
+                data[[i]] = list(probs = probs[,,rng], K = K[[i]],
+                                 markers = markers[rng,])
+
+        } # for(i)
+
+        rm(probs, K, markers)
+
+        setwd = outdir
+
+        ##
+        result = vector("list", length(data))
+        names(result) = names(data)
+
+        females = which(pheno$sex == "0")
+        males = which(pheno$sex == "1")
+
+        permutations = matrix(1, nrow = perms, ncol = 2, dimnames = list(1:perms, c("A", "X")))
+        sanger.dir = sanger.dir
+        for(p in 1:perms) {
+                LODtime = Sys.time()
+                print(p)
+                new.order = rep(0, length(surv))
+                new.order[females] = sample(females)
+                new.order[males] = sample(males)
+
+                log.perm = surv[new.order]
+                surv = log.perm
+
+                phenonew = data.frame(cbind("sex" = pheno$sex, surv))
+
+                min.a.pv = 1
+
+                for(i in 1:length(chr)) {
+                        result = GRSD.coxph4perms(data[[i]], pheno = phenonew, pheno.col, surv, addcovar, tx, sanger.dir)
+                        min.a.pv = min(min.a.pv, min(result$pv))
+                } #for(i)
+
+                min.a.pv = min(min.a.pv, min(result$pv))
+                min.x.pv = 1
+
+                if(Xchr) {
+                        result = GRSDcoxph.xchr4perms(data[["X"]], pheno = phenonew, pheno.col, surv, days.col, addcovar, tx, sanger.dir)
+                        min.x.pv = min(result$pv)
+                }
+
+                # Save the minimum p-values.
+                permutations[p,] = c(-log10(min.a.pv), -log10(min.x.pv))
+                print(paste("Max randomized LOD was", -log10(min.a.pv), -log10(min.x.pv)))
+                print(paste(round(difftime(Sys.time(), LODtime, units = 'mins'), digits = 2),
+                            "minutes..."))
+
+        }
+        print(paste(round(difftime(Sys.time(), begin, units = 'hours'), digits = 2),
+                    "hours elapsed during permutation analysis"))
+
+        save(permutations, file.prefix, file = paste0(file.prefix, "_perms.Rdata"))
+        return(permutations)
+
+
+} #GRSDcoxph.perms
---
+++ R/GRSDcoxph.xchr4perms.R
@@ -0,0 +1,113 @@
+#' Association mapping with survival outcomes using the CoxPH model.
+#' @author Elijah F Edmondson, \email{elijah.edmondson@@gmail.com}
+#'
+#' Performs association mapping in multiparent mouse populations.
+#' @export
+
+GRSDcoxph.xchr4perms = function(obj, pheno, pheno.col, surv, addcovar, tx, sanger.dir) {
+
+        chr = obj$markers[1,2]
+
+        strains = sub("/", "_", hs.colors[,2])
+
+        file.prefix = paste(tx, pheno.col, sep = "_")
+
+        plot.title = paste(tx, pheno.col, sep = " ")
+
+        load(file = paste0(sanger.dir, "X.Rdata"))
+
+        null.mod = coxph(surv ~ addcovar)
+        null.ll = logLik(null.mod)
+        pv = rep(0, nrow(sanger))
+
+        # Get the unique SDPs between each pair of markers and
+        # calculate the COXPH LOD.
+
+        # CoxPH function.
+        coxph.fxn = function(snp.rng, local.probs) {
+
+                # Get the SDPs.
+                sdp.nums = sanger[snp.rng,] %*% 2^(7:0)
+                sdps2keep = which(!duplicated(sdp.nums))
+                cur.sdps = sanger[snp.rng,,drop = FALSE][sdps2keep,,drop = FALSE]
+                unique.sdp.nums = sdp.nums[sdps2keep]
+                m = match(sdp.nums, unique.sdp.nums)
+
+                # Multiply the SDPs by the haplotype probabilities.
+                cur.alleles = tcrossprod(cur.sdps, local.probs)
+                cur.ll = rep(null.ll, nrow(cur.sdps))
+
+                # Check for low allele frequencies and remove SDPs with too
+                # few samples carrying one allele.
+                sdps.to.use = which(rowSums(cur.alleles) > 2.0)
+
+                sex.col = which(colnames(addcovar) == "sex")
+                if(length(sex.col) != 1) {
+                        stop("One of the columns of addcovar MUST be named 'sex'.")
+                } # if(length(sex.col) != 1)
+
+                # Run the Cox PH model at each unique SDP.
+                for(j in sdps.to.use) {
+
+                        ###DMG
+                        ### Put the logistic regression or linear model here.
+
+                        # For the X chromosome we map with sex as an interactive
+                        # covariate with genotype.
+                        mod = coxph(surv ~ addcovar + cur.alleles[j,] +
+                                            addcovar[,sex.col] * cur.alleles[j,])
+                        cur.ll[j] = logLik(mod)
+
+                } # for(j)
+
+                # This is the LRS.
+                cur.ll = cur.ll - null.ll
+
+                # Return the results.
+                cur.ll[m]
+
+        } # coxph.fxn()
+
+        # SNPs before the first marker.
+        snp.rng = which(sanger.hdr$POS <= obj$markers[1,3])
+        if(length(snp.rng) > 0) {
+
+                pv[snp.rng] = coxph.fxn(snp.rng, obj$probs[,,1])
+
+        } # if(length(snp.rng) > 0)
+
+        # SNPs between Markers.
+        for(i in 1:(nrow(obj$markers)-1)) {
+
+                snp.rng = which(sanger.hdr$POS > obj$markers[i,3] &
+                                        sanger.hdr$POS <= obj$markers[i+1,3])
+
+                if(length(snp.rng) > 0) {
+
+                        # Take the mean of the haplotype probs at the surrounding markers.
+                        pv[snp.rng] = coxph.fxn(snp.rng, (obj$probs[,,i] +
+                                                                  obj$probs[,,i+1]) * 0.5)
+
+                } # if(length(snp.rng) > 0)
+
+        } # for(i)
+
+        # SNPs after the last marker.
+        snp.rng = which(sanger.hdr$POS > obj$markers[nrow(obj$markers),3])
+        if(length(snp.rng) > 0) {
+
+                pv[snp.rng] = coxph.fxn(snp.rng, obj$probs[,,nrow(obj$markers)])
+
+        } # if(length(snp.rng) > 0)
+
+        # Convert LRS to p-values using the chi-squared distribution.
+        # Note that we have more degrees of freedom in the model.
+        pv = pchisq(2 * pv, df = 1, lower.tail = FALSE)
+        pv = data.frame(sanger.hdr, pv, stringsAsFactors = FALSE)
+
+
+
+        # Return the positions and p-values.
+        return(pv)
+
+} # GRSDcoxph.xchr4perms()
