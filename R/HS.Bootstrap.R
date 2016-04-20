e6bf45e824a1556dac0a7f929a89acd74faaf007
elijahedmondson <elijah.edmondson@gmail.com>
2016-04-20 12:25:19 -0600

    Bootstrap resampling

--- R/HS.Bootstrap.R
+++ R/HS.Bootstrap.R
@@ -16,9 +16,9 @@ HS.assoc.bootstrap = function(perms, chr, pheno, pheno.col, probs, K, addcovar,
                  tx = "", sanger.dir = "~/Desktop/R/QTL/WD/HS.sanger.files/")
 {
                 begin <- Sys.time()
-                file.prefix = paste(tx, pheno.col, sep = "_")
-                plot.title = paste(tx, pheno.col, sep = " ")
-                print(paste(plot.title, "Bootstrap Analysis:", Sys.time()))
+                file.prefix = paste0("Bootstrap.", tx, ".", pheno.col, "(Chr", chr, ")")
+                plot.title = paste("Bootstrap ", tx, " ", pheno.col, " ", "(Chr ", chr, "):", sep = "")
+                print(paste(plot.title, Sys.time()))
 
                 samples = intersect(rownames(pheno), rownames(probs))
                 samples = intersect(samples, rownames(addcovar))
@@ -29,61 +29,74 @@ HS.assoc.bootstrap = function(perms, chr, pheno, pheno.col, probs, K, addcovar,
                 addcovar = addcovar[samples,,drop = FALSE]
                 probs = probs[samples,,,drop = FALSE]
 
-
-
-                # LOGISTIC REGRESSION MODEL #
                 for(i in 1:length(K)) {
-                        K[[i]] = K[[i]][samples, samples]
-                } # for(i)
+                        K[[i]] = K[[i]][samples,samples,drop = FALSE]
+                }
 
-                chrs = c(chr)
-                data = vector("list", length(chr))
-                names(data) = chrs
-
-                rng = which(markers[,2] == chrs[chr])
-                data[[chr]] = list(probs = probs[,,rng], K = K[[chr]],
-                                         markers = markers[rng,])
-
-
-                data = pheno[data(nrow(data), replace = TRUE), ]
-
-                rm(probs, K, markers)
 
                 setwd(outdir)
 
                 ##
-                result = vector("list", length(data))
-                names(result) = names(data)
-
-                result = GRSDbinom.permsfast(data[[chr]], pheno = pheno, pheno.col = "Thyroid", addcovar, tx, sanger.dir)
-
 
                 permutations = matrix(1, nrow = perms, ncol = 2, dimnames = list(1:perms, c("min", "max")))
                 sanger.dir = sanger.dir
+
                 for(p in 1:perms) {
                         LODtime = Sys.time()
                         print(p)
 
-                        phenoperm = data.frame(pheno[sample(nrow(pheno), replace = TRUE), ], row.names = pheno$rownames, check.names = FALSE, check.rows = FALSE)
+                        phenoperm = data.frame(pheno[sample(nrow(pheno), replace = TRUE), ],
+                                               check.names = FALSE, check.rows = FALSE)
 
-                        # FROM DG #
+                        ### FROM DG ###
+
                         samples = sub("\\.[0-9]$", "", rownames(phenoperm))
                         probsperm = probs[samples,,]
                         rownames(probsperm) = make.unique(rownames(probsperm))
 
+                        Kperm = K
+                        for(i in 1:length(K)) {
+                                Kperm[[i]] = Kperm[[i]][samples,samples,drop = FALSE]
+                        }
 
-                        result = GRSDbinom.permsfast(data[[chr]], pheno = phenoperm, pheno.col = "pheno.col", addcovar, tx, sanger.dir)
+
+                        for(i in 1:length(K)) {
+                                rownames(Kperm[[i]]) = make.unique(rownames(Kperm[[i]]))
+                        }
+
+                        ### Move the model into the loop LOGISTIC REGRESSION MODEL ###
+                        for(i in 1:length(Kperm)) {
+                                Kperm[[i]] = Kperm[[i]][samples, samples]
+                        } # for(i)
+
+                        chrs = c(1:19, "X")
+                        data = vector("list", length(chrs))
+                        names(data) = chrs
+                        for(i in 1:length(chrs)) {
+
+                                rng = which(markers[,2] == chrs[i])
+                                data[[i]] = list(probsperm = probsperm[,,rng], Kperm = Kperm[[i]],
+                                                 markers = markers[rng,])
+
+                        } # for(i)
+                        result = vector("list", length(data))
+                        names(result) = names(data)
+                        rm(probsperm, Kperm)
+                        ### WORK HORSE ###
+
+                        result = GRSDbinom.permsfast(data[[chr]], pheno = phenoperm,
+                                                     pheno.col, addcovar, tx, sanger.dir)
 
                         top <- max(-log10(result$pv))
                         MAX.LOD = result$POS[which(-log10(result$pv) >= top)]
 
-                        print(paste0("Maximum LOD score on Chr ", chr, " is ", top, ","))
-                        print(paste("located between", max(MAX.LOD), "and ", min(MAX.LOD), "bp."))
+                        print(paste0("Maximum LOD score on Chr ", chr, " is ", top))
+                        print(paste("located between", min(MAX.LOD), "and ", max(MAX.LOD), "bp."))
 
                         #if(chr == "X") {
-                                result = GRSDbinom.xchr.permsfast(data[["X"]], pheno = phenonew, pheno.col, addcovar, tx, sanger.dir)
-                                min.x.pv = min(result$pv)
-                        }
+                        #        result = GRSDbinom.xchr.permsfast(data[["X"]], pheno = phenonew, pheno.col, addcovar, tx, sanger.dir)
+                        #        min.x.pv = min(result$pv)
+                        #}
 
                         # Save the locus.
                         permutations[p,] = c(min(MAX.LOD), max(MAX.LOD))
@@ -91,6 +104,7 @@ HS.assoc.bootstrap = function(perms, chr, pheno, pheno.col, probs, K, addcovar,
                                     "minutes..."))
 
                 }
+
                 print(paste(round(difftime(Sys.time(), begin, units = 'hours'), digits = 2),
                             "hours elapsed during analysis"))
 
@@ -101,3 +115,4 @@ HS.assoc.bootstrap = function(perms, chr, pheno, pheno.col, probs, K, addcovar,
                 return(QUANTILE)
 
 }
+
